12fab96c759b51b17397c233963f1220c18f69ce
Ruslan <Giovingo92@gmail.com>
2024-09-05 20:50:20 +0300

    fix: input styles

--- src/components/input-text.tsx
+++ src/components/input-text.tsx
@@ -10,18 +10,16 @@ export interface InputTextProps {
 
 export default function InputText({ type, id, placeholder }: InputTextProps) {
   return (
-    <div className='rounded border border-[#D1D1D6] bg-[#FFF] px-3 py-2'>
-      <div className='flex items-center gap-2 self-stretch'>
-        <Search className='shrink-0 cursor-pointer' />
-        <input
-          type={type}
-          id={id}
-          placeholder={placeholder}
-          className='w-full outline-none'
-        />
-        <Help className='shrink-0 cursor-pointer' />
-        <ShortKey />
-      </div>
+    <div className='flex items-center gap-2 self-stretch rounded border border-[#D1D1D6] bg-[#FFF] px-3 py-2'>
+      <Search className='shrink-0 cursor-pointer' />
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        className='w-full outline-none'
+      />
+      <Help className='shrink-0 cursor-pointer' />
+      <ShortKey />
     </div>
   );
 }
